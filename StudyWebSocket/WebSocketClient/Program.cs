ae6f4ddcbd9385900d818d047d4f74af2017e453
Tetsuo Honda <t-honda@hondarer-soft.com>
2020-04-20 12:48:57 +0900

    Initial commit: サンプルの動作確認

---
+++ StudyWebSocket/WebSocketClient/Program.cs
@@ -0,0 +1,78 @@
+﻿// C#でWebSocketのサンプルを動かしてみた
+// https://qiita.com/Zumwalt/items/53797b0156ebbdcdbfb1
+
+using System;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WebSocketClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Run().Wait();
+        }
+
+        static async Task Run()
+        {
+            //クライアント側のWebSocketを定義
+            ClientWebSocket ws = new ClientWebSocket();
+
+            //接続先エンドポイントを指定
+            Uri uri = new Uri("ws://localhost:8000/ws/");
+
+            //サーバに対し、接続を開始
+            await ws.ConnectAsync(uri, CancellationToken.None);
+            byte[] buffer = new byte[1024];
+
+            //情報取得待ちループ
+            while (true)
+            {
+                //所得情報確保用の配列を準備
+                ArraySegment<byte> segment = new ArraySegment<byte>(buffer);
+
+                //サーバからのレスポンス情報を取得
+                WebSocketReceiveResult result = await ws.ReceiveAsync(segment, CancellationToken.None);
+
+                //エンドポイントCloseの場合、処理を中断
+                if (result.MessageType == WebSocketMessageType.Close)
+                {
+                    await ws.CloseAsync(WebSocketCloseStatus.NormalClosure, "OK",
+                      CancellationToken.None);
+                    return;
+                }
+
+                //バイナリの場合は、当処理では扱えないため、処理を中断
+                if (result.MessageType == WebSocketMessageType.Binary)
+                {
+                    await ws.CloseAsync(WebSocketCloseStatus.InvalidMessageType,
+                      "I don't do binary", CancellationToken.None);
+                    return;
+                }
+
+                //メッセージの最後まで取得
+                int count = result.Count;
+                while (!result.EndOfMessage)
+                {
+                    if (count >= buffer.Length)
+                    {
+                        await ws.CloseAsync(WebSocketCloseStatus.InvalidPayloadData,
+                          "That's too long", CancellationToken.None);
+                        return;
+                    }
+                    segment = new ArraySegment<byte>(buffer, count, buffer.Length - count);
+                    result = await ws.ReceiveAsync(segment, CancellationToken.None);
+
+                    count += result.Count;
+                }
+
+                //メッセージを取得
+                string message = Encoding.UTF8.GetString(buffer, 0, count);
+                Console.WriteLine("> " + message);
+            }
+        }
+    }
+}
---
+++ StudyWebSocket/WebSocketServer/Program.cs
@@ -0,0 +1,64 @@
+﻿// C#でWebSocketのサンプルを動かしてみた
+// https://qiita.com/Zumwalt/items/53797b0156ebbdcdbfb1
+
+using System;
+using System.Net;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WebSocketServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Run().Wait();
+        }
+
+        static async Task Run()
+        {
+            //Httpリスナーを立ち上げ、クライアントからの接続を待つ
+            HttpListener s = new HttpListener();
+            s.Prefixes.Add("http://localhost:8000/ws/");
+            s.Start();
+            HttpListenerContext hc = await s.GetContextAsync();
+
+            //クライアントからのリクエストがWebSocketでない場合は処理を中断
+            if (!hc.Request.IsWebSocketRequest)
+            {
+                //クライアント側にエラー(400)を返却し接続を閉じる
+                hc.Response.StatusCode = 400;
+                hc.Response.Close();
+                return;
+            }
+
+            //WebSocketでレスポンスを返却
+            HttpListenerWebSocketContext wsc = await hc.AcceptWebSocketAsync(null);
+            WebSocket ws = wsc.WebSocket;
+
+            //１０回のレスポンスを返却
+            for (int i = 0; i != 10; ++i)
+            {
+                //1回のレスポンスごとに2秒のウエイトを設定
+                await Task.Delay(2000);
+
+                //レスポンスのテストメッセージとして、現在時刻の文字列を取得
+                string time = DateTime.Now.ToLongTimeString();
+
+                //文字列をByte型に変換
+                byte[] buffer = Encoding.UTF8.GetBytes(time);
+                ArraySegment<byte> segment = new ArraySegment<byte>(buffer);
+
+                //クライアント側に文字列を送信
+                await ws.SendAsync(segment, WebSocketMessageType.Text,
+                  true, CancellationToken.None);
+            }
+
+            //接続を閉じる
+            await ws.CloseAsync(WebSocketCloseStatus.NormalClosure,
+              "Done", CancellationToken.None);
+        }
+    }
+}
