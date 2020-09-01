32b3e8218e62f20709234f576b923610e8947ec1
Tetsuo Honda <t-honda@hondarer-soft.com>
2020-09-01 22:37:57 +0900

    不要なインターフェース宣言を削除

--- StudyWebSocket/Hondarersoft.WebInterface/IWebInterfaceService.cs
+++ StudyWebSocket/Hondarersoft.WebInterface/IWebInterfaceService.cs
@@ -7,14 +7,6 @@ namespace Hondarersoft.WebInterface
 {
     public interface IWebInterfaceService
     {
-        public string Hostname { get; set; }
-
-        public int PortNumber { get; set; }
-
-        public string BasePath { get; set; }
-
-        public bool UseSSL { get; set; }
-
         public Task StartAsync();
     }
 }
